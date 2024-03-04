4cab91610fb9dd652dbb7c8ca50a992d3b3092d1
ikozlitin <ikozlitin@elvees.com>
2024-03-04 18:58:18 +0300

    Пишем автоанотатор

--- pyapitest.py
+++ pyapitest.py
@@ -2,7 +2,8 @@ from ultralytics import YOLO
 from PIL import Image
 
 model = YOLO("drone8_14.pt")
-results = model.predict(source="/home/nkozlitin/Videos/555/1.mp4", save_txt=True, stream=True, save_frames=True, save=True, show_boxes=False)
+#results = model.predict(source="/home/ivan/videos/555/1.mp4", save_txt=True, stream=True, save_frames=True, save=True, show_boxes=False)
+results = model.predict(source="/home/ivan/videos/555/1.mp4", save_txt=False, stream=True, save_frames=False, save=False, show_boxes=False)
 
-for result in results:
-    pass
+for index,result in enumerate(results):
+    result.save_txt(f'xxx/{index}.txt')
