9eef08e945034665e6ef3120b32bcdf4682bd405
ikozlitin <ikozlitin@elvees.com>
2024-03-05 16:50:02 +0300

    Сделал сохранение в формате yolo

--- pyapitest.py
+++ pyapitest.py
@@ -1,9 +1,15 @@
 from ultralytics import YOLO
 from PIL import Image
+from pathlib import Path
 
 model = YOLO("drone8_14.pt")
 #results = model.predict(source="/home/ivan/videos/555/1.mp4", save_txt=True, stream=True, save_frames=True, save=True, show_boxes=False)
-results = model.predict(source="/home/ivan/videos/555/1.mp4", save_txt=False, stream=True, save_frames=False, save=False, show_boxes=False)
+results = model.predict(source="/home/nkozlitin/cvdatastore/VideoArchive/MainData/LeftObjects/certification/AO_LO_2_TOP.avi", save_txt=False, stream=True, save_frames=False, save=False, show_boxes=False)
+
+Path("xxx/images").mkdir(parents=True, exist_ok=True)
+Path("xxx/labels").mkdir(parents=True, exist_ok=True)
 
 for index,result in enumerate(results):
-    result.save_txt(f'xxx/{index}.txt')
+    result.save_txt(f'xxx/labels/{index}.txt')
+    im = Image.fromarray(result.orig_img, mode="RGB")
+    im.save(f'xxx/images/{index}.jpg')
