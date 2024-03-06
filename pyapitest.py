6b64fd53ddf4ba8ac1013190744a0249d7114910
ikozlitin <ikozlitin@elvees.com>
2024-03-06 12:05:24 +0300

    Подправил xxx.yaml

--- pyapitest.py
+++ pyapitest.py
@@ -2,9 +2,9 @@ from ultralytics import YOLO
 from PIL import Image
 from pathlib import Path
 
-model = YOLO("drone8_14.pt")
+model = YOLO("yolov8n.pt")
 #results = model.predict(source="/home/ivan/videos/555/1.mp4", save_txt=True, stream=True, save_frames=True, save=True, show_boxes=False)
-results = model.predict(source="ultralytics/assets/bus.jpg", save_txt=False, stream=True, save_frames=False, save=True, show_boxes=True)
+results = model.predict(source="/home/nkozlitin/video/aaa.mp4", save_txt=False, stream=True, save_frames=False, save=True, show_boxes=True, conf=0.1)
 
 Path("xxx/images").mkdir(parents=True, exist_ok=True)
 Path("xxx/labels").mkdir(parents=True, exist_ok=True)
