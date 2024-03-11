f7389b8b8d4db57eefaf0b4b3e313ac0edb33b3a
ikozlitin <ikozlitin@elvees.com>
2024-03-11 12:09:28 +0300

    Анализ разности

--- new_objects_view.py
+++ new_objects_view.py
@@ -22,30 +22,40 @@ if __name__ == '__main__':
 
     lb_stack=[]
     lb_stack_size = 150
+    image_stack=[]
     for path_,txt_ in zip(paths, txts):
         image = cv2.imread(str(path_))
-        annotator = Annotator(image, line_width=3)
+        image_stack.append(image.copy())
+        if(len(image_stack) > lb_stack_size):
+            image_stack.pop(0)
         
+        image_bottom = image_stack[0]
+        diff_image  = ((image.astype(np.float32) - image_bottom.astype(np.float32)) / 2 + 128).astype(np.uint8)
+
         try:
             with open(txt_) as f:
                 lb = [x.split() for x in f.read().strip().splitlines() if len(x)]
-                lb = np.array(lb, dtype=np.float32)
-                #
-                lb_stack.append(lb)
-                if(len(lb_stack) > lb_stack_size):
-                    lb_stack.pop(0)
-                #
-                xyxy_top = torch.tensor(xywhn2xyxy(lb[:, 1:5], w=image.shape[1], h=image.shape[0]))
-                xyxy_bottom = torch.tensor(xywhn2xyxy(lb_stack[0][:, 1:5], w=image.shape[1], h=image.shape[0]))
-                iou_matrix = box_iou(xyxy_top, xyxy_bottom)
-                max_ious, _ = torch.max(iou_matrix,axis=1)
-                print(max_ious)
+                lb = [lb_item for lb_item in lb if int(lb_item[0]) != 0]
+                if len(lb):
+                    lb = np.array(lb, dtype=np.float32)
+                    #
+                    lb_stack.append(lb)
+                    if(len(lb_stack) > lb_stack_size):
+                        lb_stack.pop(0)
+                    #
+                    
+                    xyxy_top = torch.tensor(xywhn2xyxy(lb[:, 1:5], w=image.shape[1], h=image.shape[0]))
+                    xyxy_bottom = torch.tensor(xywhn2xyxy(lb_stack[0][:, 1:5], w=image.shape[1], h=image.shape[0]))
+                    iou_matrix = box_iou(xyxy_top, xyxy_bottom)
+                    max_ious, _ = torch.max(iou_matrix,axis=1)
+                    print(max_ious)
+                    
+                    annotator = Annotator(diff_image, line_width=3)
+                    for cls, xyxy_item, max_iou in zip(lb[:,0],xyxy_top, max_ious):
+                        #if(max_iou > 0.3):
+                        annotator.box_label(xyxy_item, f"{int(cls)}", color=colors((max_iou > 0.3) * 3, True))
 
-                for cls, xyxy_item, max_iou in zip(lb[:,0],xyxy_top, max_ious):
-                    if(max_iou < 0.1 and cls != 0):
-                        annotator.box_label(xyxy_item, f"{int(cls)}", color=colors(cls, True))
-
-                cv2.imshow("image",image)
+                cv2.imshow("image",diff_image)
                 print(image.shape)
                 print(f'labels_top: {xyxy_top}')
                 print(f'lables_bottom: {xyxy_bottom}')
