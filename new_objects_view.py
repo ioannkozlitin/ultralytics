5cd1f29470c3d9b956bf4e9660f6b4c1b4994fb0
ikozlitin <ikozlitin@elvees.com>
2024-03-13 12:38:46 +0300

    Делаю выгрузку датасета в файл

--- new_objects_view.py
+++ new_objects_view.py
@@ -48,20 +48,22 @@ if __name__ == '__main__':
         xyxy_bottom = torch.tensor(xywhn2xyxy(lb_stack[0][:, 1:5], w=image.shape[1], h=image.shape[0]))
         iou_matrix = box_iou(xyxy_top, xyxy_bottom)
         max_ious, _ = torch.max(iou_matrix,axis=1)
-        print(max_ious)
+        #print(max_ious)
                 
         image_bottom = image_stack[0]
         diff_image  = ((image.astype(np.float32) - image_bottom.astype(np.float32)) / 2 + 128).astype(np.uint8)
         annotator = Annotator(diff_image, line_width=3)
         for cls, xyxy_item, max_iou in zip(lb[:,0],xyxy_top, max_ious):
-            annotator.box_label(xyxy_item, f"{int(cls)}", color=colors((max_iou > 0.3) * 3, True))
+            if xyxy_item[0] <= image.shape[1]:
+                iou_condition = int(max_iou > 0.3)
+                annotator.box_label(xyxy_item, f"{int(cls)}", color=colors(iou_condition * 3, True))
 
         cv2.imshow("image",diff_image)
-        print(image.shape)
-        print(f'labels_top: {xyxy_top}')
-        print(f'lables_bottom: {xyxy_bottom}')
-        print(f'image path: {path_}')
-        print(f'image annotation: {txt_}')
+        #print(image.shape)
+        #print(f'labels_top: {xyxy_top}')
+        #print(f'lables_bottom: {xyxy_bottom}')
+        #print(f'image path: {path_}')
+        #print(f'image annotation: {txt_}')
 
         if cv2.waitKey(1)==27:
             break
