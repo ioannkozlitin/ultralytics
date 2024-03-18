f3732a9a106688c1af007e368619da41ac9f91b0
ikozlitin <ikozlitin@elvees.com>
2024-03-18 16:22:39 +0300

    CVRES-573 Добавил возможность аугментации в датасет

--- my_dataloader.py
+++ my_dataloader.py
@@ -2,23 +2,10 @@
 
 import torch
 import torch.utils.data
-import torchvision as tv
 import cv2
 import numpy
-import os
-import random
-from pathlib import PurePosixPath
 from pathlib import Path
-import itertools
-
-import yaml
-import json
-import sys
-
 import torchvision.transforms as T
-from torchvision.transforms.functional import to_pil_image
-import multiprocessing as mp
-
 from ultralytics.utils.ops import xywhn2xyxy
 
 class MyDataset(torch.utils.data.Dataset):
@@ -28,6 +15,7 @@ class MyDataset(torch.utils.data.Dataset):
         with open(labels_filename) as f:
             self.spec_labels = numpy.array([[float(x_item) for x_item in x.split()] for x in f.read().strip().splitlines() if len(x)])
 
+        self.root_dataset_path = Path(labels_filename).parent
         self.new_size = new_size
         self.transform = transform
 
@@ -35,8 +23,8 @@ class MyDataset(torch.utils.data.Dataset):
     def __getitem__(self, item):
         spec_label = self.spec_labels[item]
 
-        i_bottom = root_path / "images" / Path(str(int(spec_label[0]))+".jpg")
-        i_top = root_path / "images" / Path(str(int(spec_label[1]))+".jpg")
+        i_bottom = self.root_dataset_path / "images" / Path(str(int(spec_label[0]))+".jpg")
+        i_top = self.root_dataset_path / "images" / Path(str(int(spec_label[1]))+".jpg")
         top_image = cv2.imread(str(i_top))
         bottom_image = cv2.imread(str(i_bottom))
         labels = [int(x) for x in xywhn2xyxy(spec_label[2:6], w=top_image.shape[1], h=top_image.shape[0])]
@@ -53,7 +41,7 @@ if __name__ == "__main__":
 
     transforms = T.Compose([
         T.RandomRotation(degrees=(-15, 15)),
-        T.RandomResizedCrop((128, 128)),
+        T.RandomResizedCrop((128, 128), antialias=True),
         T.GaussianBlur(9)]
     )
 
