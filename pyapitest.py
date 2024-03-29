db3c983304156e6df95169309e1bbf0dfd3f5041
ikozlitin <ikozlitin@elvees.com>
2024-03-29 10:57:12 +0300

    Добавил формирование videos.txt

--- pyapitest.py
+++ pyapitest.py
@@ -20,14 +20,12 @@ def process_video(video_item):
             im_resized = im.resize((image_width, image_height))
             im_resized.save(str(image_path / f'{index}.jpg'))
             f.write(str(image_path / f'{index}.jpg')+'\n')
-            if index > 100:
-                break
     
     return str(root_dataset_folder / txt_file_name)
 
 if __name__ == "__main__":
-    root_folder = Path("/home/nkozlitin/cvdatastore/VideoArchive/MainData")
-    root_dataset_folder = Path("xxx")
+    root_folder = Path("/home/ivan/cvdatastore/VideoArchive/MainData")
+    root_dataset_folder = Path("/home/ivan/mnt/Video/xxx")
     video_list=["LeftObjects/certification/AO_LO_4_SIDE.avi","LeftObjects/certification/AO_LO_3_SIDE.avi","LeftObjects/certification/AO_LO_2_SIDE.avi"]
     image_width = 640
     yolo_nn_name = "luggage8_16.pt"
@@ -40,6 +38,7 @@ if __name__ == "__main__":
 
     with multiprocessing.Pool(None) as pool:
         videos = pool.map(process_video, video_list)
-        print(videos)
-
+        with open(root_dataset_folder / "videos.txt", "w") as f:
+            for video in videos:
+                f.write(video+'\n')
 
