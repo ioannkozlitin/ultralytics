b72c76f662f8f935fcc480f82c29a3a56d0a6b62
Иван Алексеевич Козлитин <kozlitin@elins.ru>
2024-10-10 09:46:41 +0300

    Начало работ на конвертором xml->yolov8

---
+++ viewxmlannotation.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+from annotation import Annotation
+from ultralytics.utils.plotting import Annotator, colors
+from ultralytics.utils.ops import xyxy2xywhn
+
+if __name__ == '__main__':
+    annname = "/home/neuron-2/Видео/new/DJI_2024_09_20_15_23_21/annotations.xml"
+    videoname = "/home/neuron-2/Видео/new/2024_09_20_15_23_21_visual_narrow.mp4"
+
+    annotation = Annotation()
+    annotation.load(annname)
+    print(f'{len(annotation.frame_poses)}')
+
+    cap = cv2.VideoCapture(videoname)
+
+    while True:
+        frame_number = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+        current_annotation = annotation.get_frame_poses(frame_number)
+
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        annotator = Annotator(frame, line_width=3)
+        frame_annotations=list()
+        for pos in current_annotation:
+            xywhn = xyxy2xywhn(np.array([pos.x1, pos.y1, pos.x2, pos.y2]), w = annotation.original_width, h = annotation.original_height)
+            track_label = annotation.labels[pos.track.label]
+            lxywhn = xywhn.tolist()
+            lxywhn.insert(0,track_label)
+            frame_annotations.append(lxywhn)
+            annotator.box_label((pos.x1, pos.y1, pos.x2, pos.y2), f"{track_label}", color=colors(0, True))
+
+        print(frame_annotations)
+
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
