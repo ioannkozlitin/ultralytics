37ab142670bd7f83cf9bbecdb3ba209ed0d48129
Иван Алексеевич Козлитин <kozlitin@elins.ru>
2024-10-10 11:29:25 +0300

    Сделал вчерне преобразование в yolov8

--- viewxmlannotation.py
+++ viewxmlannotation.py
@@ -5,6 +5,7 @@ from ultralytics.utils.plotting import Annotator, colors
 from ultralytics.utils.ops import xyxy2xywhn
 from pathlib import Path
 from shutil import rmtree
+import random
 
 if __name__ == '__main__':
     annname = "/home/neuron-2/Видео/new/DJI_2024_09_20_15_23_21/annotations.xml"
@@ -21,10 +22,10 @@ if __name__ == '__main__':
 
     annotation = Annotation()
     annotation.load(annname)
-    print(f'{len(annotation.frame_poses)}')
 
     cap = cv2.VideoCapture(videoname)
 
+    images_list = []
     while True:
         frame_number = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
         current_annotation = annotation.get_frame_poses(frame_number)
@@ -32,6 +33,10 @@ if __name__ == '__main__':
         ret, frame = cap.read()
         if not ret:
             break
+
+        framejpg = f'{images_path}/{frame_number}.jpg'
+        cv2.imwrite(framejpg, frame)
+        images_list.append(f"./images/{frame_number}.jpg")
 
         annotator = Annotator(frame, line_width=3)
         frame_annotations=list()
@@ -43,11 +48,30 @@ if __name__ == '__main__':
             frame_annotations.append(lxywhn)
             annotator.box_label((pos.x1, pos.y1, pos.x2, pos.y2), f"{track_label}", color=colors(0, True))
 
-        print(frame_annotations)
+        if len(current_annotation) > 0:
+            anntxt = f'{labels_path}/{frame_number}.txt'
+            with open(anntxt,"wt") as f:
+                for ann_item in frame_annotations:
+                    for el in ann_item:
+                        print(el, file=f, end=' ')
+                    print(file=f)
 
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) == 27:
             break
+        
+    nfold = 5
+    k = 2
+    ftr = open( root_dataset_folder / "train.txt", 'w' )
+    fte = open( root_dataset_folder / "validation.txt", 'w' )
+    for image_name in images_list:
+        if random.randint( 1 , nfold ) == k:
+            print(image_name, file=fte)
+        else:
+            print(image_name, file=ftr)
+    
+    ftr.close()
+    fte.close()
 
     cap.release()
     cv2.destroyAllWindows()
