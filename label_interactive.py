4ed5078400a9ef3a224de327b7c08eb750d0e652
Иван Алексеевич Козлитин <kozlitin@elins.ru>
2024-12-03 16:35:43 +0300

    [#619078] Долавил кнопки проигрывания, пошаговый режим

--- label_interactive.py
+++ label_interactive.py
@@ -20,12 +20,23 @@ class Core:
         self.canvas = Canvas(self.root, width=width+100, height=height, bg='white')
         self.delay = 5
         self.play = False
+        self.reverse = False
+        self.step = False
+        self.frame_number = 0
+        #
+        self.startpos_button = ttk.Button(text="StartPos", command=self.fstartpos)
+        self.play_button = ttk.Button(text="Play", command=self.fplay)
+        self.reverse_button = ttk.Button(text="Back", command=self.freverse)
+        self.step_button = ttk.Button(text="Step", command=self.fstep)
+        self.frame_label = ttk.Label(text="0")
+        #
+        self.startpos_button.place(x = width + 5, y = 5)
+        self.play_button.place(x = width + 5, y = 40)
+        self.reverse_button.place(x = width + 5, y = 75)
+        self.step_button.place(x = width + 5, y = 110)
+        self.frame_label.place(x = width + 5, y = height - 30)
+        #
         self.loop()
-        #
-        ttk.Button(text="Start", command=self.start).place(x = width + 5, y = 5)
-        ttk.Button(text="Play", command=self.fplay).place(x = width + 5, y = 35)
-        ttk.Button(text="Stop", command=self.fstop).place(x = width + 5, y = 65)
-        #
         self.canvas.pack()
         self.root.mainloop()
 
@@ -33,9 +44,12 @@ class Core:
         self.cap.release()
         
     def next_frame(self):
-        frame_number = int(self.cap.get(cv2.CAP_PROP_POS_FRAMES))
+        if self.frame_number != int(self.cap.get(cv2.CAP_PROP_POS_FRAMES)):
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.frame_number)
+
         ret, frame = self.cap.read()
         if ret:
+            self.frame_number = int(self.cap.get(cv2.CAP_PROP_POS_FRAMES))
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR) 
             self.image = ImageTk.PhotoImage(Image.fromarray(frame))
             self.canvas.create_image(1, 1, anchor=NW, image=self.image)
@@ -44,18 +58,29 @@ class Core:
     def loop(self):
         if self.play:
             self.play = self.next_frame()
+            if self.reverse:
+                self.frame_number = max(self.frame_number - 2, 0)
+            if self.step:
+                self.play = False
+                self.step = False
+        self.frame_label.config(text = str(self.frame_number))
+        self.play_button.config(text="Stop" if self.play else "Play")
+        self.reverse_button.config(text="Forward" if self.reverse else "Back")
         self.root.after(self.delay, self.loop)
 
-    def start(self):
-        self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        self.fplay()
-
     def fplay(self):
+        self.play = not self.play
+
+    def freverse(self):
+        self.reverse = not self.reverse
+    
+    def fstep(self):
+        self.step = True
         self.play = True
 
-
-    def fstop(self):
-        self.play = False
+    def fstartpos(self):
+        self.frame_number = 0
+        self.freverse = False
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
